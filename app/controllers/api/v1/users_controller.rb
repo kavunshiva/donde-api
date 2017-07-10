da4d357c61573c9ad952a9b3df5657465f9e48f1
kavunshiva <kavunshiva@gmail.com>
2017-07-09 20:34:01 -0400

    modify users_controller to render jwt token on signup

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -9,7 +9,11 @@ class Api::V1::UsersController < ApplicationController
   def create
     user = User.new(user_params)
     if user.save
-      render json: user
+      render json: {
+        id: user.id,
+        username: user.username,
+        jwt: JWT.encode({user_id: user.id}, ENV['JWT_SECRET'], ENV['JWT_ALGORITHM'])
+      }
     else
       render json: [{}], status: 404
     end
